c2c3d08eb0b2c17fdb4a4e228e87b63163592e68
pat2903 <103569848+pat2903@users.noreply.github.com>
2024-02-23 12:44:09 +0000

    theory of general relativity - ayush rochlani

--- turn.py
+++ turn.py
@@ -9,6 +9,8 @@ motorB = LargeMotor('outB')
 motorC = LargeMotor('outC')
 steering_motor = MediumMotor('outA')
 
+steering_motor.run_to_abs_pos(position_sp=0, speed_sp=500)
+
 # To run both motors at the same speed.
 motorB.run_forever(speed_sp=-500)
 motorC.run_forever(speed_sp=-500)
@@ -18,7 +20,7 @@ steering_motor.run_to_rel_pos(position_sp=90, speed_sp=500)
 
 sleep(1)
 
-steering_motor.run_to_rel_pos(position_sp=0, speed_sp=500)
+steering_motor.run_to_rel_pos(position_sp=-90, speed_sp=500)
 
 sleep(1)
 
@@ -27,5 +29,7 @@ motorC.stop(stop_action="coast")
 
 # # To straighten the wheel, turn it 45 degrees to the right.
 # steering_motor.run_to_rel_pos(position_sp=-90, speed_sp=500)
+
+steering_motor.run_to_abs_pos(position_sp=0, speed_sp=500)
 
 steering_motor.stop(stop_action="coast")
