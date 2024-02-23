aea02eaf61e0c0e59d028fd9fe4d3938d4f460ca
pat2903 <103569848+pat2903@users.noreply.github.com>
2024-02-23 12:35:52 +0000

    come on roch

--- turn.py
+++ turn.py
@@ -15,17 +15,24 @@ motorC.run_forever(speed_sp=-500)
 
 sleep(1)
 
-# Stop both motors.
 motorB.stop(stop_action="coast")
 motorC.stop(stop_action="coast")
 
 # positive angle turns left
 steering_motor.run_to_rel_pos(position_sp=45, speed_sp=500)
 
-sleep(3)
+sleep(1)
+
+motorB.run_forever(speed_sp=-500)
+motorC.run_forever(speed_sp=-500)
+
+sleep(1)
+
+motorB.stop(stop_action="coast")
+motorC.stop(stop_action="coast")
 
 # To straighten the wheel, turn it 45 degrees to the right.
-steering_motor.run_to_rel_pos(position_sp=90, speed_sp=500)
+steering_motor.run_to_rel_pos(position_sp=-90, speed_sp=500)
 
 sleep(1) 
 
