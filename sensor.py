9de6830fdf9d82a59e685337773fe0568f3ef84d
Ayush <86850534+aroch17@users.noreply.github.com>
2024-02-24 01:46:29 +0000

    Create getter func for ir sensor value

--- sensor.py
+++ sensor.py
@@ -1,10 +1,2 @@
-#!/usr/bin/env python3
-
-import ev3dev.ev3 as ev3 
-from time import sleep
-
-ir = ev3.InfraredSensor('in1') 
-ir.mode = 'IR-PROX' 
-
-for i in range(100):
-    print(ir.value()) 
+def get_ir_value(ir_sensor) -> int:
+    return ir_sensor.value()
