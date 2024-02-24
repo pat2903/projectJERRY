039cfc112ae09dfe0cbacabb90b970994dd93b70
Ayush <86850534+aroch17@users.noreply.github.com>
2024-02-24 01:48:05 +0000

    Create helper funcs for moving

--- move.py
+++ move.py
@@ -1,6 +1,13 @@
-#!/usr/bin/env python3 
-import ev3dev.ev3 as ev3 
+def move_motor_forward_timed(motor, time, speed) -> None:
+    """Move motor forward by specified time and speed. Positive speed moves backward."""
+    motor.run_timed(time_sp=time, speed_sp=speed)
 
-m = ev3.LargeMotor('outB')
 
-m.run_timed(speed_sp=300, time_sp=1000)
+def move_motor_forever(motor, speed) -> None:
+    """Moves motor forever. Positive speed moves backward."""
+    motor.run_forever(speed_sp=speed)
+
+
+def stop_motor(motor, action: str) -> None:
+    """Stops motor with the specified action"""
+    motor.stop(stop_action=action)
