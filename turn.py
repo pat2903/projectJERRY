f2128e882cdab1e48523a16a6e7ef6a3f88f1a0c
Ayush <86850534+aroch17@users.noreply.github.com>
2024-02-26 18:34:13 +0000

    Update reset pos

--- turn.py
+++ turn.py
@@ -1,11 +1,11 @@
 def reset_steering(steering_motor) -> None:
     """Makes steering motor face forward"""
-    steering_motor.run_to_abs_pos(position_sp=-50, speed_sp=500)
+    steering_motor.run_to_abs_pos(position_sp=-80, speed_sp=500)
 
 
 def turn_steering(steering_motor, angle: int, speed:int) -> None:
     """Turn steering motor by specified angle and specified speed. Positive angle turns left."""
-    steering_motor.run_to_abs_pos(position_sp=angle, speed_sp=speed)
+    steering_motor.run_to_rel_pos(position_sp=angle, speed_sp=speed)
     # why are we not using abs_pos here?
 
 def get_current_steering_motor_pos(steering_motor) -> int:
