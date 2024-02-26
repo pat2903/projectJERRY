178fc8cdb7e52acc35d57a22116d8645f0f3a4eb
Ayush <86850534+aroch17@users.noreply.github.com>
2024-02-26 18:02:57 +0000

    Update run and detect func

--- detect.py
+++ detect.py
@@ -47,30 +47,26 @@ def run_and_detect(steering_motor, motor1, motor2, ir_sensor) -> None:
 
     is_running = True
     while is_running:
-        # if get_ir_value(ir_sensor) <= 10:
-        #     reset_steering(steering_motor)
-        #     sleep(1)
-        #     stop_motor(motor1, action="brake")
-        #     stop_motor(motor2, action="brake")
-        #     state = "s"
-        #     is_running = False
-        
-        # while state == "t":
-        #     if get_ir_value(ir_sensor) > 50:
-        #         reset_steering(steering_motor)
-        #         state = "f"
-        #     elif get_ir_value(ir_sensor) <= 10:
-        #         reset_steering(steering_motor)
-        #         sleep(1)
-        #         stop_motor(motor1, action="brake")
-        #         stop_motor(motor2, action="brake")
-        #         state = "s"
-        #         is_running = False
+        if state == "s":
+            reset_steering(steering_motor)
+            sleep(1)
+            stop_motor(motor1, action="brake")
+            stop_motor(motor2, action="brake")
+            is_running = False
 
-        # while state == "f":
-        #     if get_ir_value(ir_sensor) <= 50:
-        #         turn_steering(steering_motor, angle=90, speed=500)
-        #         state = "t"
+        while state == "t":
+            if get_ir_value(ir_sensor) <= 10:
+                state = "s"
+            elif get_ir_value(ir_sensor) > 50:
+                reset_steering(steering_motor)
+                state = "f"
+
+        while state == "f":
+            if get_ir_value(ir_sensor) <= 10:
+                state = "s"
+            elif get_ir_value(ir_sensor) <= 50:
+                turn_steering(steering_motor, angle=90, speed=500)
+                state = "t"
         
 
         reset_steering(steering_motor)
