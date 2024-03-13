75cef3182d08bf1b810734666c6cfe633f913c29
pat2903 <103569848+pat2903@users.noreply.github.com>
2024-03-13 11:30:28 +0000

    update canoe turn speed

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ def main() -> None:
     motorC = LargeMotor('outC')
 
     # run_and_detect(steering_motor, motorB, motorC, ir_sensor)
-    # canoe_turn(steering_motor, motorB, motorC)
+    canoe_turn(steering_motor, motorB, motorC)
 
-    turn_steering(steering_motor, -90, 100)
-    move_motor_forward_timed(motorB, 5000, -400)
-    move_motor_forward_timed(motorC, 5000, -400)
+    # turn_steering(steering_motor, -90, 100)
+    # move_motor_forward_timed(motorB, 5000, -400)
+    # move_motor_forward_timed(motorC, 5000, -400)
     
 
 
