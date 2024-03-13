80418f9c094fe5be5cdf98cbff315ca49452e5f3
pat2903 <103569848+pat2903@users.noreply.github.com>
2024-03-13 11:53:38 +0000

    add colour printing

--- colour.py
+++ colour.py
@@ -1,17 +1,20 @@
 import ev3dev.ev3 as ev3
+from typing import final
 
-# 0: No colour
-# 1: Black
-# 2: Blue
-# 3: Green
-# 4: Yellow
-# 5: Red
-# 6: White
-# 7: Brown
+colour_codes : final = {
+    0: 'No colour',
+    1: 'Black',
+    2: 'Blue',
+    3: 'Green',
+    4: 'Yellow',
+    5: 'Red',
+    6: 'White',
+    7: 'Brown'
+}
 
 def colour_detector():
     colour_sensor = ev3.ColorSensor('in4')
     colour_sensor.mode = 'COL-COLOR'
     while(colour_sensor.color != colour_sensor.COLOR_BLUE):
-        colour = colour_sensor.value()
-        print(colour)
+        colour_value = colour_sensor.value()
+        print("Detected " + colour_codes[colour_value])
