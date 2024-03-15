ee1360e823171eec68852ba62215b446962180f3
Alice Graham <s2142414@sparkles.inf.ed.ac.uk>
2024-03-15 14:09:36 +0000

    Add gyro sensor

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import ev3dev.ev3 as ev3
 from ev3dev2.motor import LargeMotor, MediumMotor
+from time import sleep
 
 from detect import run_and_detect
 from canoe_turn import canoe_turn
@@ -16,12 +17,17 @@ def main() -> None:
     # ir_sensor.mode = 'IR-PROX'
 
     # initialise US sensor
-    us_sensor = ev3.UltrasonicSensor('in1') 
-    us_sensor.mode = 'US-DIST_CM'
+    us_sensor = ev3.UltrasonicSensor('in2') 
+    us_sensor.mode = 'US-DIST-CM'
 
-    # # initialise gyro sensor
-    # us_sensor = ev3.UltrasonicSensor('in1') 
-    # us_sensor.mode = 
+    # initialise gyro sensor
+    gyro_sensor = ev3.GyroSensor('in4') 
+    gyro_sensor.mode = 'GYRO-RATE'
+    # sleep(1.5)
+    gyro_sensor.mode = 'GYRO-ANG'
+    # gyro_sensor.angle = 0
+    # gyro_sensor.reset()
+    # sleep(1)
 
     # Initialize the motors
     steering_motor = MediumMotor('outA')
@@ -30,8 +36,9 @@ def main() -> None:
 
     # run_and_detect(steering_motor, motorB, motorC, us_sensor)
 
-    while (us_sensor.value()>10):
-        print(us_sensor.value())
+    while (us_sensor.value()>50):
+        print(gyro_sensor.value())
+
     
 
 
