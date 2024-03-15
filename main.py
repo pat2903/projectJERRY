70e138265bd1d31e03f60d385feccd894aa25101
Ayush <86850534+aroch17@users.noreply.github.com>
2024-03-15 23:08:46 +0000

    Clean up main

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
 #!/usr/bin/env python3
 
 import ev3dev.ev3 as ev3
-from ev3dev2.motor import LargeMotor, MediumMotor
+from ev3dev2.motor import LargeMotor
 from time import sleep
 
 from detect import run_and_detect
-from canoe_turn import canoe_turn
-from move import move_motor_forward_timed
-from turn import turn_steering
-from colour import colour_detector
+from pivot_turn import pivot_turn
+from colour import detect_colour
             
 
 def main() -> None:
-    # initialise IR sensor
-    # ir_sensor = ev3.InfraredSensor('in1') 
-    # ir_sensor.mode = 'IR-PROX'
+    # initialize the motors
+    motorB = LargeMotor('outB')
+    motorC = LargeMotor('outC')
+
+    # initialise gyro sensor
+    gyro_sensor = ev3.GyroSensor('in4')
+    
+    # reset gyro angle to 0
+    gyro_sensor.mode = 'GYRO-RATE'
+    gyro_sensor.mode = 'GYRO-ANG'
+
+    # initialise colour sensor
+    colour_sensor = ev3.ColorSensor('in4')
+    colour_sensor.mode = 'COL-COLOR'
 
     # initialise US sensor
     # us_sensor = ev3.UltrasonicSensor('in2') 
     # us_sensor.mode = 'US-DIST-CM'
 
-    # # initialise gyro sensor
-    gyro_sensor = ev3.GyroSensor('in4') 
-    gyro_sensor.mode = 'GYRO-RATE'
-    # sleep(1.5)
-    gyro_sensor.mode = 'GYRO-ANG'
-    # gyro_sensor.angle = 0
-    # gyro_sensor.reset()
-    # sleep(1)
-
-    # Initialize the motors
-    # steering_motor = MediumMotor('outA')
-    motorB = LargeMotor('outB')
-    motorC = LargeMotor('outC')
-
-    canoe_turn(motorB, motorC, 90, gyro_sensor)
+    pivot_turn(motorB, motorC, gyro_sensor, 90)
 
     # run_and_detect(steering_motor, motorB, motorC, us_sensor)
-
-    # while (us_sensor.value()>50):
-    #     print(gyro_sensor.value())
-
-    
 
 
 if __name__ == "__main__":
