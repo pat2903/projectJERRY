b091b076c93995b36ef509bff99ea7fb8dbba85f
pat2903 <103569848+pat2903@users.noreply.github.com>
2024-03-15 14:12:30 +0000

    turn

--- canoe_turn.py
+++ canoe_turn.py
@@ -2,10 +2,7 @@ from turn import turn_steering, reset_steering, get_current_steering_motor_pos
 from move import move_motor_forever, move_motor_forward_timed, stop_motor
 from time import sleep
 
-def canoe_turn(steering_motor, motorB, motorC):
-    turn_steering(steering_motor, -90,  100)
+def canoe_turn(motorB, motorC):
+    move_motor_forward_timed(motorB, 5000, -200)
+    move_motor_forward_timed(motorC, 5000, 200)
     sleep(2)
-    move_motor_forward_timed(motorB, 5000, -400)
-    move_motor_forward_timed(motorC, 5000, 400)
-    sleep(2)
-    reset_steering(steering_motor)
--- main.py
+++ main.py
@@ -17,27 +17,29 @@ def main() -> None:
     # ir_sensor.mode = 'IR-PROX'
 
     # initialise US sensor
-    us_sensor = ev3.UltrasonicSensor('in2') 
-    us_sensor.mode = 'US-DIST-CM'
+    # us_sensor = ev3.UltrasonicSensor('in2') 
+    # us_sensor.mode = 'US-DIST-CM'
 
-    # initialise gyro sensor
-    gyro_sensor = ev3.GyroSensor('in4') 
-    gyro_sensor.mode = 'GYRO-RATE'
-    # sleep(1.5)
-    gyro_sensor.mode = 'GYRO-ANG'
+    # # initialise gyro sensor
+    # gyro_sensor = ev3.GyroSensor('in4') 
+    # gyro_sensor.mode = 'GYRO-RATE'
+    # # sleep(1.5)
+    # gyro_sensor.mode = 'GYRO-ANG'
     # gyro_sensor.angle = 0
     # gyro_sensor.reset()
     # sleep(1)
 
     # Initialize the motors
-    steering_motor = MediumMotor('outA')
+    # steering_motor = MediumMotor('outA')
     motorB = LargeMotor('outB')
     motorC = LargeMotor('outC')
 
+    canoe_turn(motorB, motorC)
+
     # run_and_detect(steering_motor, motorB, motorC, us_sensor)
 
-    while (us_sensor.value()>50):
-        print(gyro_sensor.value())
+    # while (us_sensor.value()>50):
+    #     print(gyro_sensor.value())
 
     
 
