32e6a8d65668fa23920e1734e6a9ec786f0f1eea
pat2903 <103569848+pat2903@users.noreply.github.com>
2024-03-15 14:35:23 +0000

    increase speed and reduce tolerance

--- canoe_turn.py
+++ canoe_turn.py
@@ -5,12 +5,12 @@ import ev3dev.ev3 as ev3
 
 def canoe_turn(motorB, motorC, turn_angle, gyro: ev3.GyroSensor):
     target_angle = gyro.value() + turn_angle
-    move_motor_forever(motorB, -150)
-    move_motor_forever(motorC, 150)
+    move_motor_forever(motorB, -200)
+    move_motor_forever(motorC, 200)
     print("Target angle: %s", target_angle)
     while True:
         print("Current angle: %s", gyro.value())
-        if target_angle - 3 <= gyro.value() <= target_angle + 3:
+        if target_angle - 2 <= gyro.value() <= target_angle + 2:
             stop_motor(motorB, "brake")
             stop_motor(motorC, "brake")
             break
