7c0ba7f53f838ed527cf8dfd8fefc9a203c29dd1
Ayush <86850534+aroch17@users.noreply.github.com>
2024-03-15 23:05:31 +0000

    Add getters for gyro and colour sensor

--- sensor.py
+++ sensor.py
@@ -1,2 +1,8 @@
 def get_ir_value(ir_sensor) -> int:
     return ir_sensor.value()
+
+def get_current_gyro_angle(gyro_sensor) -> int:
+    return gyro_sensor.value()
+
+def get_colour_sensor_value(colour_sensor) -> int:
+    return colour_sensor.value()
